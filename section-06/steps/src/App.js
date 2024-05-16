870cdb5212ef1097f5760076f092778d1e48edc5
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-16 09:36:35 +0200

    :sparkles: add state to button events

--- section-06/steps/src/App.js
+++ section-06/steps/src/App.js
@@ -1,5 +1,43 @@
+import { messages } from './messages';
+import { useState } from 'react';
+
 function App() {
-  return <div>Hello React</div>;
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step !== 1) setStep(step - 1);
+  };
+
+  const handleNext = () => {
+    if (step !== 3) setStep(step + 1);
+  };
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
