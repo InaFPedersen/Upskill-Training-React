3e5140332c7d6d0a0109f547fba297b4d94485a2
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-16 06:37:58 +0200

    :sparkles: using map method on the pizza list

--- section-05/pizza-menu/src/components/Menu.jsx
+++ section-05/pizza-menu/src/components/Menu.jsx
@@ -1,10 +1,18 @@
+import { pizzaData } from '../pizzaData';
 import Pizza from './Pizza';
 
 const Menu = () => {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -15,7 +23,7 @@ const Menu = () => {
         ingredients="Bread with italian olive oil and rosemary"
         photoName="pizzas/focaccia.jpg"
         price={6}
-      />
+      /> */}
     </main>
   );
 };
--- section-05/pizza-menu/src/components/Pizza.jsx
+++ section-05/pizza-menu/src/components/Pizza.jsx
@@ -1,13 +1,13 @@
 export default function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
