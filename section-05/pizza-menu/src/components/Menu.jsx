7a46a74d1d8ae8d5c8e18aa4dfaf5d7367ad5862
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-16 07:31:48 +0200

    :recycle: adding and changing to conditional rendering

--- section-05/pizza-menu/src/components/Menu.jsx
+++ section-05/pizza-menu/src/components/Menu.jsx
@@ -2,17 +2,32 @@ import { pizzaData } from '../pizzaData';
 import Pizza from './Pizza';
 
 const Menu = () => {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later!</p>
+      )}
 
-      {/* <Pizza
+      {/* 
+      FOR SHOWING ONE BY ONE, OLD SCHOOL
+      <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
