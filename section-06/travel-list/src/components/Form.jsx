1cb3ad5204f06c2d3695dd325004d655e76ef72b
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-21 07:42:18 +0200

    :sparkles: add form component and form logic

--- section-06/travel-list/src/components/Form.jsx
+++ section-06/travel-list/src/components/Form.jsx
@@ -1,3 +1,42 @@
-const Form = () => {};
+import { useState } from 'react';
+
+const Form = () => {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+};
 
 export default Form;
--- section-06/travel-list/src/components/PackingList.jsx
+++ section-06/travel-list/src/components/PackingList.jsx
@@ -1,9 +1,16 @@
-import PackingItem from './PackingItem';
+import { initialItems } from '../initialItems';
+import Item from './Item';
 
 const PackingList = () => {
-  <div>
-    <PackingItem />
-  </div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PackingList;
