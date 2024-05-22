a178829848b9eca40ed0b15282254fbe3a9cc5f7
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-22 07:37:08 +0200

    :sparkles: add reusable message component

--- section-07/steps/src/components/Steps.jsx
+++ section-07/steps/src/components/Steps.jsx
@@ -1,6 +1,7 @@
 import { messages } from '../messages';
 import { useState } from 'react';
 import Button from './Button';
+import StepMessage from './StepMessage';
 
 const Steps = () => {
   const [step, setStep] = useState(1);
@@ -27,24 +28,9 @@ const Steps = () => {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
-            {/* <Button
-              textColor="#fff"
-              backgroundColor="#7950f2"
-              onClick={handlePrevious}
-              text="Previous"
-              emoji="👈"
-            />
-            <Button
-              textColor="#fff"
-              backgroundColor="#7950f2"
-              onClick={handleNext}
-              text="Next"
-              emoji="👉"
-            /> */}
             <Button
               textColor="#fff"
               backgroundColor="#7950f2"
