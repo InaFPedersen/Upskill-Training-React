9c26d4607c5e33106be60421d77703f1280a430c
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-30 08:36:58 +0200

    :sparkles: add useRef to search

--- part-2/section-13/usepopcorn/src/components/Search.jsx
+++ part-2/section-13/usepopcorn/src/components/Search.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useRef } from 'react';
+
 const Search = ({ query, onQuery }) => {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === 'Enter') {
+          inputEl.current.focus();
+          onQuery('');
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+      return () => document.addEventListener('keydown', callback);
+    },
+    [onQuery]
+  );
+
   return (
     <input
       className="search"
@@ -6,6 +26,7 @@ const Search = ({ query, onQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => onQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
