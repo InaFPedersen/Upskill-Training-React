0c41ef3a3e55e3967f651518d31614dcf8762828
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-30 13:53:29 +0200

    :sparkles: add weather app

---
+++ part-2/section-14/classy-weather/src/Day.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function formatDay(dateStr) {
+  return new Intl.DateTimeFormat('en', {
+    weekday: 'short',
+  }).format(new Date(dateStr));
+}
+
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], '☀️'],
+    [[1], '🌤'],
+    [[2], '⛅️'],
+    [[3], '☁️'],
+    [[45, 48], '🌫'],
+    [[51, 56, 61, 66, 80], '🌦'],
+    [[53, 55, 63, 65, 57, 67, 81, 82], '🌧'],
+    [[71, 73, 75, 77, 85, 86], '🌨'],
+    [[95], '🌩'],
+    [[96, 99], '⛈'],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return 'NOT FOUND';
+  return icons.get(arr);
+}
+
+class Day extends React.Component {
+  render() {
+    const { date, max, min, code, isToday } = this.props;
+
+    return (
+      <li className="day">
+        <span>{getWeatherIcon(code)}</span>
+        <p>{isToday ? 'Today' : formatDay(date)}</p>
+        <p>
+          {min}&deg; &mdash; {max}&deg;
+        </p>
+      </li>
+    );
+  }
+}
+
+export default Day;
