05d444eb236578f3e0dd7ff32038d8823feb699f
Ina F. Pedersen <inapedersen90@gmail.com>
2024-05-31 13:48:45 +0200

    :sparkles: add a progress bar and point system

--- part-3/section-16/react-quiz/src/components/App.js
+++ part-3/section-16/react-quiz/src/components/App.js
@@ -5,13 +5,18 @@ import Loader from './Loader';
 import Error from './Error';
 import Question from './Question';
 import StartScreen from './StartScreen';
+import NextButton from './NextButton';
 import { useEffect, useReducer } from 'react';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
 
   // loading, error, ready, active, finished state
   status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -32,15 +37,34 @@ function reducer(state, action) {
         ...state,
         status: 'active',
       };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error(' Action is unknown');
   }
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numberOfQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch('http://localhost:9000/questions')
@@ -62,7 +86,23 @@ function App() {
             dispatch={dispatch}
           />
         )}
-        {status === 'active' && <Question />}
+        {status === 'active' && (
+          <>
+            <Progress
+              index={index}
+              numberOfQuestions={numberOfQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
 
       <aside>
--- part-3/section-16/react-quiz/src/components/Question.js
+++ part-3/section-16/react-quiz/src/components/Question.js
@@ -1,5 +1,15 @@
-function Question() {
-  return <div>Question</div>;
+import Options from './Options';
+
+function Question({ question, dispatch, answer }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+
+      <div className="options">
+        <Options question={question} dispatch={dispatch} answer={answer} />
+      </div>
+    </div>
+  );
 }
 
 export default Question;
