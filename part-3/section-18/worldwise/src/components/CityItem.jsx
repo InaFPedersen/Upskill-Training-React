6978d2477e42bc3bae8ef1658217c74614872222
Ina F. Pedersen <inapedersen90@gmail.com>
2024-06-04 13:47:50 +0200

    :sparkles: adding the worldwise app base

---
+++ part-3/section-18/worldwise/src/components/CityItem.jsx
@@ -0,0 +1,38 @@
+import styles from './CityItem.module.css';
+import { Link } from 'react-router-dom';
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date(date));
+
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join('');
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
+function CityItem({ city }) {
+  const { cityName, emoji, date, id, position } = city;
+
+  return (
+    <li>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
