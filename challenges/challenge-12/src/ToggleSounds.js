121829dafc3508573715087e8c2c2f200e700deb
Ina F. Pedersen <inapedersen90@gmail.com>
2024-06-10 06:34:22 +0200

    :star: add challenge 12 create a workout timer

--- challenges/challenge-12/src/ToggleSounds.js
+++ challenges/challenge-12/src/ToggleSounds.js
@@ -1,12 +1,14 @@
+import { memo } from 'react';
+
 function ToggleSounds({ allowSound, setAllowSound }) {
   return (
     <button
       className="btn-sound"
       onClick={() => setAllowSound((allow) => !allow)}
     >
-      {allowSound ? "🔈" : "🔇"}
+      {allowSound ? '🔈' : '🔇'}
     </button>
   );
 }
 
-export default ToggleSounds;
+export default memo(ToggleSounds);
