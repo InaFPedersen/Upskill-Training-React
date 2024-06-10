f78c7b67ce3a945416a7c567a010e70c6307ef2f
Ina F. Pedersen <inapedersen90@gmail.com>
2024-06-10 07:19:06 +0200

    :recycle: update code to update the title to how many exercises is chosen

--- part-3/section-19/workout-timer/src/Calculator.js
+++ part-3/section-19/workout-timer/src/Calculator.js
@@ -28,6 +28,13 @@ function Calculator({ workouts, allowSound }) {
     [allowSound, duration]
   );
 
+  useEffect(
+    function () {
+      document.title = `Your ${number}-exercise workout`;
+    },
+    [number]
+  );
+
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
