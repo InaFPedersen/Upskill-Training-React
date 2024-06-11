5fb45e4a7b085169752c6cf015e1b9db2029d3cb
Ina F. Pedersen <inapedersen90@gmail.com>
2024-06-11 13:53:19 +0200

    :sparkles: add order and search orders

--- part-4/section-22/fast-react-pizza/src/App.jsx
+++ part-4/section-22/fast-react-pizza/src/App.jsx
@@ -5,7 +5,7 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import Order from './features/order/Order';
+import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
 // If you are going to fetch data and etc. this is the method you have to use to create a router
@@ -23,7 +23,12 @@ const router = createBrowserRouter([
       },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
-      { path: '/order/:orderId', element: <Order /> },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
