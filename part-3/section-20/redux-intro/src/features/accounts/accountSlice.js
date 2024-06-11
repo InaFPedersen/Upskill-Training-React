9de7d10e4a68df4b3992c56eaf4756e63cb94c19
Ina F. Pedersen <inapedersen90@gmail.com>
2024-06-11 07:08:58 +0200

    :sparkles: add and use redux toolkit

--- part-3/section-20/redux-intro/src/features/accounts/accountSlice.js
+++ part-3/section-20/redux-intro/src/features/accounts/accountSlice.js
@@ -1,47 +1,50 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case 'account/deposit':
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case 'account/withdraw':
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case 'account/payLoan':
-      // if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      };
-    case 'account/convertingCurrency':
-      return { ...state, isLoading: true };
-    default:
-      return state;
-  }
-}
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
 
-// export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = '';
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
 export function deposit(amount, currency) {
   if (currency === 'USD') return { type: 'account/deposit', payload: amount };
@@ -59,53 +62,4 @@ export function deposit(amount, currency) {
   };
 }
 
-export function withdraw(amount) {
-  return { type: 'account/withdraw', payload: amount };
-}
-
-export function requestLoan(amount, purpose) {
-  return {
-    type: 'account/requestLoan',
-    payload: { amount, purpose },
-  };
-}
-
-export function payLoan() {
-  return { type: 'account/payLoan' };
-}
-
-/// TO TEST THE FUNCTIONALITY!!!! ///
-// store.dispatch({ type: 'account/deposit', payload: 500 });
-
-// console.log('Hallo from store.js deposit functionality');
-// console.log(store.getState());
-
-// store.dispatch({ type: 'account/withdraw', payload: 300 });
-
-// console.log('Hallo from store.js withdraw functionality');
-// console.log(store.getState());
-
-// store.dispatch({
-//   type: 'account/requestLoan',
-//   payload: { amount: 1000, purpose: 'Buy a mini cooper car!' },
-// });
-
-// console.log('Hallo from store.js requestLoan functionality');
-// console.log(store.getState());
-
-// store.dispatch({ type: 'account/payLoan' });
-
-// console.log('Hallo from store.js payLoan functionality');
-// console.log(store.getState());
-
-// store.dispatch(deposit(500));
-// console.log(store.getState());
-
-// store.dispatch(withdraw(200));
-// console.log(store.getState());
-
-// store.dispatch(requestLoan(1000, 'Buy a cheap and solid car'));
-// console.log(store.getState());
-
-// store.dispatch(payLoan());
-// console.log(store.getState());
+export default accountSlice.reducer;
--- part-3/section-20/redux-intro/src/features/customers/customerSlice.js
+++ part-3/section-20/redux-intro/src/features/customers/customerSlice.js
@@ -1,42 +1,37 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   fullName: '',
   nationalID: '',
   createdAt: '',
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case 'customer/createCustomer':
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case 'customer/updateName':
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-    default:
-      return state;
-  }
-}
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: 'customer/createCustomer',
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
+export const { createCustomer, updateName } = customerSlice.actions;
 
-export function updateName(fullName) {
-  return { type: 'customer/updateName', payload: fullName };
-}
-
-/// TO TEST THE FUNCTIONALITY!!!! ///
-// store.dispatch(createCustomer('Nora Nordmann', '01010101'));
-// console.log(store.getState());
-
-// store.dispatch(updateName('Nora Nordsjø'));
-// console.log(store.getState());
+export default customerSlice.reducer;
