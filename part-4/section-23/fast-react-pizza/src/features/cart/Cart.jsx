ad8cbc3f27ffadf96271936e62cd3dd75df5344b
Ina F. Pedersen <ina.pedersen@bouvet.no>
2024-06-17 07:53:26 +0200

    :art: add styling to the cart

--- part-4/section-23/fast-react-pizza/src/features/cart/Cart.jsx
+++ part-4/section-23/fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -31,16 +32,20 @@ function Cart() {
   console.log(cart);
 
   return (
-    <div>
+    <div className='px-4 py-3'>
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
 
-      <div>
+      <ul className='mt-3 divide-y divide-stone-300 border-b'>
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
 
-        <Button type='primary' to="/order/new">Order pizzas</Button>
-        
-        <button>Clear cart</button>
+      <div className='mt-6 space-x-2'>
+        <Button type='primary' to="/order/new">Order pizzas</Button>       
+        <Button type='secondary'>Clear cart</Button>
       </div>
     </div>
   );
--- part-4/section-23/fast-react-pizza/src/ui/Button.jsx
+++ part-4/section-23/fast-react-pizza/src/ui/Button.jsx
@@ -5,7 +5,8 @@ function Button({children, disabled, to, type}) {
 
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
-    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    secondary: 'border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 focus:text-stone-800 active:bg-stone-300 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
   }
 
   if(to) return <Link to={to} className={styles[type]}>{children}</Link>
