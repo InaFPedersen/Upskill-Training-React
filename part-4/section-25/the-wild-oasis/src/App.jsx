21de73b2f4ba3895c972e5d6f0004ea704e3270b
Ina F. Pedersen <ina.pedersen@bouvet.no>
2024-06-18 08:52:51 +0200

    :art: add styled components and global styles

--- part-4/section-25/the-wild-oasis/src/App.jsx
+++ part-4/section-25/the-wild-oasis/src/App.jsx
@@ -1,5 +1,32 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: aliceblue;
+`;
+
+const StyledApp = styled.div`
+  background-color: #ccc;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World!!!!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Checking in...')}>Check in</Button>
+        <Button onClick={() => alert('Checking out...')}>Check out</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
