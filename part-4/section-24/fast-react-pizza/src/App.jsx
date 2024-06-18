d6d5588b6ebd87cdeab995bce1ee90b0666b4e4a
Ina F. Pedersen <ina.pedersen@bouvet.no>
2024-06-18 07:29:47 +0200

    :recycle: refactor code so that you can add priority tag at a later stage

--- part-4/section-24/fast-react-pizza/src/App.jsx
+++ part-4/section-24/fast-react-pizza/src/App.jsx
@@ -8,6 +8,7 @@ import CreateOrder, {
   action as createOrderAction,
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
+import {action as updateOrderAction} from './features/order/UpdateOrder';
 import AppLayout from './ui/AppLayout';
 
 // If you are going to fetch data and etc. this is the method you have to use to create a router
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
--- part-4/section-24/fast-react-pizza/src/features/order/Order.jsx
+++ part-4/section-24/fast-react-pizza/src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
 
 import OrderItem from './OrderItem';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const order = useLoaderData();
@@ -68,6 +69,8 @@ function Order() {
         {priority && <p className='text-sm font-medium text-stone-700'>Price priority: {formatCurrency(priorityPrice)}</p>}
         <p className='font-bold'>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
+
+      {!priority && <UpdateOrder order={order} /> }
     </div>
   );
 }
