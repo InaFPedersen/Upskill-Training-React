ac546566e73e22039bc6f8981828f6e5a3c89746
Ina F. Pedersen <ina.pedersen@bouvet.no>
2024-06-18 07:14:24 +0200

    :sparkles: add fetching data using useFetcher

--- part-4/section-24/fast-react-pizza/src/features/order/Order.jsx
+++ part-4/section-24/fast-react-pizza/src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
@@ -9,44 +9,16 @@ import {
 } from '../../utils/helpers';
 
 import OrderItem from './OrderItem';
-
-// const order = {
-//   id: 'ABCDEF',
-//   customer: 'Jonas',
-//   phone: '123456789',
-//   address: 'Arroios, Lisbon , Portugal',
-//   priority: true,
-//   estimatedDelivery: '2027-04-25T10:00:00',
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: 'Napoli',
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: 'Diavola',
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: 'Romana',
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: '-9.000,38.000',
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
+import { useEffect } from 'react';
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(function () {
+    if(!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu')
+  }, [fetcher])
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -81,7 +53,14 @@ function Order() {
       </div>
 
       <ul className='divide-y divide-stone-300 border-b border-t'>
-        {cart.map((item) => <OrderItem item={item} key={item.pizzaId} />)}
+        {cart.map((item) => (
+          <OrderItem 
+            item={item} 
+            key={item.pizzaId} 
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={fetcher?.data?.find(element => element.id === item.pizzaId)?.ingredients ?? []} 
+          />
+        ))}
       </ul>
 
       <div className='space-y-2 bg-stone-300 px-6 py-5'>
