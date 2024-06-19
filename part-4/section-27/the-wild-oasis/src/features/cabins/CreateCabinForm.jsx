2e37ab6464a39e19dba988733f49624f4278d309
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-06-19 13:15:08 +0200

    :recycle: refactor and clean up imports

--- part-4/section-27/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ part-4/section-27/the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
 
 import Input from '../../ui/Input';
 import Form from '../../ui/Form';
@@ -7,8 +8,6 @@ import Button from '../../ui/Button';
 import FileInput from '../../ui/FileInput';
 import Textarea from '../../ui/Textarea';
 import FormRow from '../../ui/FormRow';
-
-import { useForm } from 'react-hook-form';
 import { createCabin } from '../../services/apiCabins';
 
 function CreateCabinForm() {
