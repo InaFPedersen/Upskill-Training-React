fde4bedd75b8bdfaa92ccfad4599dcd269a61ef3
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-06-21 12:35:30 +0200

    :art: add a own styled component file for cabin row

--- part-4/section-28/the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ part-4/section-28/the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,58 +1,16 @@
-import styled from 'styled-components';
-
+import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
+import { formatCurrency } from '../../utils/helpers';
+import Modal from '../../ui/Modal';
+import Table from '../../ui/Table';
 import CreateCabinForm from './CreateCabinForm';
 import { useDeleteCabin } from './useDeleteCabin';
-import { formatCurrency } from '../../utils/helpers';
-import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
 import { useCreateCabin } from './useCreateCabin';
-import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
-import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
-
-// const TableRow = styled.div`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-//   padding: 1.4rem 2.4rem;
-
-//   &:not(:last-child) {
-//     border-bottom: 1px solid var(--color-grey-100);
-//   }
-// `;
-
-const Img = styled.img`
-  display: block;
-  width: 6.4rem;
-  aspect-ratio: 3 / 2;
-  object-fit: cover;
-  object-position: center;
-  transform: scale(1.5) translateX(-7px);
-`;
-
-const Cabin = styled.div`
-  font-size: 1.6rem;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  font-family: 'Sono';
-`;
-
-const Price = styled.div`
-  font-family: 'Sono';
-  font-weight: 600;
-`;
-
-const Discount = styled.div`
-  font-family: 'Sono';
-  font-weight: 500;
-  color: var(--color-green-700);
-`;
-
+import { Cabin, Discount, Img, Price } from './CabinRowStyledComponent';
 function CabinRow({ cabin }) {
   const { isDeleting, deleteCabin } = useDeleteCabin();
   const { isCreating, createCabin } = useCreateCabin();
-
   const {
     id: cabinId,
     name,
@@ -62,7 +20,6 @@ function CabinRow({ cabin }) {
     image,
     description,
   } = cabin;
-
   function handleDuplicate() {
     createCabin({
       name: `Copy of ${name}`,
@@ -73,9 +30,8 @@ function CabinRow({ cabin }) {
       description,
     });
   }
-
   return (
-    <Table.Row>
+    <Table.Row role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
@@ -83,31 +39,26 @@ function CabinRow({ cabin }) {
       {discount ? (
         <Discount>{formatCurrency(discount)}</Discount>
       ) : (
-        <span>&mdash;</span>
+        <span>&mdashl;</span>
       )}
       <div>
         <Modal>
           <Menus.Menu>
             <Menus.Toggle id={cabinId} />
-
             <Menus.List id={cabinId}>
               <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
                 Duplicate
               </Menus.Button>
-
               <Modal.Open opens="edit">
                 <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
               </Modal.Open>
-
               <Modal.Open opens="delete">
                 <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
               </Modal.Open>
             </Menus.List>
-
             <Modal.Window name="edit">
               <CreateCabinForm cabinToEdit={cabin} />
             </Modal.Window>
-
             <Modal.Window name="delete">
               <ConfirmDelete
                 resourceName="cabins"
@@ -121,5 +72,4 @@ function CabinRow({ cabin }) {
     </Table.Row>
   );
 }
-
 export default CabinRow;
