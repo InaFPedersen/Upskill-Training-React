f87f9460bf9783afc1d59312514343ab2c7242dc
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-06-21 08:15:18 +0200

    :recycle: refactor to use the render prop to display the table body

--- part-4/section-28/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ part-4/section-28/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -35,9 +35,11 @@ function CabinTable() {
         <div>Discount</div>
         <div></div>
       </Table.Header>
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
     </Table>
   );
 }
