eb58bb78c2a9cc9ac0b069188d4c7b14b7ea8eec
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-19 13:07:57 +0200

    :sparkles: add sorting

--- part-4/section-29/the-wild-oasis/src/features/bookings/useBookings.js
+++ part-4/section-29/the-wild-oasis/src/features/bookings/useBookings.js
@@ -5,19 +5,24 @@ import { useSearchParams } from 'react-router-dom';
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
-  // Filter
+  // FILTER
   const filterValue = searchParams.get('status');
   const filter =
     !filterValue || filterValue === 'all'
       ? null
       : { field: 'status', value: filterValue };
 
+  // SORT
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
+    queryKey: ['bookings', filter, sortBy],
     queryFn: () => getBookings({ filter }),
   });
 
