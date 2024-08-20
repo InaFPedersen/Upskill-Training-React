9d0b6dadf3da470e432826c241ac9f50fdfa2770
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-20 07:42:14 +0200

    :sparkles: add checkout

--- part-4/section-29/the-wild-oasis/src/features/bookings/BookingRow.jsx
+++ part-4/section-29/the-wild-oasis/src/features/bookings/BookingRow.jsx
@@ -1,42 +1,13 @@
-import styled from 'styled-components';
 import { format, isToday } from 'date-fns';
-
+import { Amount, Cabin, Stacked } from './BookingRowStyledComponent';
 import Tag from '../../ui/Tag';
 import Table from '../../ui/Table';
-
 import { formatCurrency } from '../../utils/helpers';
 import { formatDistanceFromNow } from '../../utils/helpers';
 import Menus from '../../ui/Menus';
-import { HiArrowDownOnSquare, HiEye } from 'react-icons/hi2';
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
-import { Menu } from '../../ui/MenusStyledComponent';
-
-const Cabin = styled.div`
-  font-size: 1.6rem;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  font-family: 'Sono';
-`;
-
-const Stacked = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.2rem;
-
-  & span:first-child {
-    font-weight: 500;
-  }
-
-  & span:last-child {
-    color: var(--color-grey-500);
-    font-size: 1.2rem;
-  }
-`;
-
-const Amount = styled.div`
-  font-family: 'Sono';
-  font-weight: 500;
-`;
+import { useCheckout } from '../check-in-out/useCheckout';
 
 function BookingRow({
   booking: {
@@ -53,6 +24,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: 'blue',
@@ -102,6 +74,16 @@ function BookingRow({
               onClick={() => navigate(`/checkin/${bookingId}`)}
             >
               Check in
+            </Menus.Button>
+          )}
+
+          {status === 'checked-in' && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => checkout(bookingId)}
+              disabled={isCheckingOut}
+            >
+              Check out
             </Menus.Button>
           )}
         </Menus.List>
---
+++ part-4/section-29/the-wild-oasis/src/features/bookings/BookingRowStyledComponent.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Cabin = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: 'Sono';
+`;
+
+export const Stacked = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+
+  & span:first-child {
+    font-weight: 500;
+  }
+
+  & span:last-child {
+    color: var(--color-grey-500);
+    font-size: 1.2rem;
+  }
+`;
+
+export const Amount = styled.div`
+  font-family: 'Sono';
+  font-weight: 500;
+`;
