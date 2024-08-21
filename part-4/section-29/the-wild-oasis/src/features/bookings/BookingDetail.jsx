22074a7b4a4a8136a01aae0978494d12c4317fb0
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-21 12:50:15 +0200

    :recycle: fix small bugs

--- part-4/section-29/the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ part-4/section-29/the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,7 @@ import { useCheckout } from '../check-in-out/useCheckout';
 import { useDeleteBooking } from './useDeleteBooking';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
+import { Empty } from '../../ui/TableStyledComponent';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -33,6 +34,8 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
+
+  if (!booking) return <Empty resourceName="booking" />;
 
   const { status, id: bookingId } = booking;
 
