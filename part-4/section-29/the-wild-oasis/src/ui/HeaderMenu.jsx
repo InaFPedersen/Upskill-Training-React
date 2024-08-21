16439b4f42051e82a0313669f13c353108964d89
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-21 08:20:47 +0200

    :sparkles: add dark-mode for app

--- part-4/section-29/the-wild-oasis/src/ui/HeaderMenu.jsx
+++ part-4/section-29/the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import Logout from '../features/authentication/Logout';
 import ButtonIcon from './ButtonIcon';
 import { HiOutlineUser } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
+import DarkModeToggle from './DarkModeToggle';
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -17,6 +18,9 @@ const HeaderMenu = () => {
         <ButtonIcon onClick={() => navigate('/account')}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
