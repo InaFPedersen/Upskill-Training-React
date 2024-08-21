981301f35f765d48bc18090ec12bdd30319a5e7e
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-21 11:11:58 +0200

    :sparkles: add functionality for showing today's activities

--- part-4/section-29/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ part-4/section-29/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,7 +35,7 @@ const DashboardLayout = () => {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Todays activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
