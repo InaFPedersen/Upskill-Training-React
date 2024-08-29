6b4cdc722d8c0d25778ffac5103b63cb47f935e1
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-29 11:58:22 +0200

    :sparkles: add  starter files to the project

--- part-5/section-33/the-wild-oasis-website/app/layout.js
+++ part-5/section-33/the-wild-oasis-website/app/layout.js
@@ -1,5 +1,5 @@
-import Logo from './components/Logo';
-import Navigation from './components/Navigation';
+import Logo from './_components/Logo';
+import Navigation from './_components/Navigation';
 
 export const metadata = {
   title: 'The Wild Oasis',
