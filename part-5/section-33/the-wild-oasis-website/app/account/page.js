f73b8e2a70d6d3c0d5e0f68072cf554a6a967456
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-30 06:31:54 +0200

    :sparkles: add styling, fonts, colors, illustrations

--- part-5/section-33/the-wild-oasis-website/app/account/page.js
+++ part-5/section-33/the-wild-oasis-website/app/account/page.js
@@ -1,7 +1,11 @@
+export const metadata = {
+  title: 'Account',
+};
+
 export default function Page() {
   return (
     <div>
-      <h1>Profile</h1>;
+      <h1>Account</h1>;
     </div>
   );
 }
--- part-5/section-33/the-wild-oasis-website/app/layout.js
+++ part-5/section-33/the-wild-oasis-website/app/layout.js
@@ -1,14 +1,27 @@
 import Logo from './_components/Logo';
 import Navigation from './_components/Navigation';
 
+import { Josefin_Sans } from 'next/font/google';
+
+import './_styles/globals.css';
+
+const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
+
 export const metadata = {
-  title: 'The Wild Oasis',
+  title: {
+    template: 'The Wild Oasis - %s',
+    default: 'The Wild Oasis - Welcome',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful nature, with a stunning view of the mountains and forests.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
