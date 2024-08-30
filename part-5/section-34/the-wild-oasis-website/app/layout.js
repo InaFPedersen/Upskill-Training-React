a88b63fbeffb7ce0ff604eabcc08fa8f0998b10f
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-08-30 09:24:29 +0200

    :sparkles: add code base for section 34

---
+++ part-5/section-34/the-wild-oasis-website/app/layout.js
@@ -0,0 +1,34 @@
+import Logo from './_components/Logo';
+import Navigation from './_components/Navigation';
+
+import { Josefin_Sans } from 'next/font/google';
+
+import './_styles/globals.css';
+import Header from './_components/Header';
+
+const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
+
+export const metadata = {
+  title: {
+    template: 'The Wild Oasis - %s',
+    default: 'The Wild Oasis - Welcome',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful nature, with a stunning view of the mountains and forests.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+        {/* <footer>© Copyright by the Wild Oasis!</footer> */}
+      </body>
+    </html>
+  );
+}
