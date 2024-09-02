79e4bb3d37c996c205abbb97307471000982f895
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-09-02 13:36:20 +0200

    :sparkles: add code base for section 35

---
+++ part-5/section-35/the-wild-oasis-website/app/account/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Account',
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Account</h1>
+      <p>Welcome, user!</p>
+    </div>
+  );
+}
---
+++ part-5/section-35/the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -0,0 +1,90 @@
+import TextExpander from '@/app/_components/TextExpander';
+import { getCabin } from '@/app/_lib/data-service';
+import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
+import Image from 'next/image';
+
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin - ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  const id = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return id;
+}
+
+export default async function Page({ params }) {
+  const cabin = await getCabin(params.cabinId);
+
+  const { id, name, maxCapacity, regularPrice, discount, image, description } =
+    cabin;
+
+  return (
+    <div className="max-w-6xl mx-auto mt-8">
+      <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
+        <div className="relative scale-[1.15] -translate-x-3">
+          <Image
+            src={image}
+            fill
+            className="object-cover"
+            alt={`Cabin ${name}`}
+          />
+        </div>
+
+        <div>
+          <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
+            Cabin {name}
+          </h3>
+
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
+
+          <ul className="flex flex-col gap-4 mb-7">
+            <li className="flex gap-3 items-center">
+              <UsersIcon className="h-5 w-5 text-primary-600" />
+              <span className="text-lg">
+                For up to <span className="font-bold">{maxCapacity}</span>{' '}
+                guests
+              </span>
+            </li>
+            <li className="flex gap-3 items-center">
+              <MapPinIcon className="h-5 w-5 text-primary-600" />
+              <span className="text-lg">
+                Located in the heart of the{' '}
+                <span className="font-bold">Dolomites</span> (Italy)
+              </span>
+            </li>
+            <li className="flex gap-3 items-center">
+              <EyeSlashIcon className="h-5 w-5 text-primary-600" />
+              <span className="text-lg">
+                Privacy <span className="font-bold">100%</span> guaranteed
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="text-5xl font-semibold text-center">
+          Reserve today. Pay on arrival.
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+// // PLACEHOLDER DATA
+// const cabin = {
+//   id: 89,
+//   name: '001',
+//   maxCapacity: 2,
+//   regularPrice: 250,
+//   discount: 0,
+//   description:
+//     'Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.',
+//   image:
+//     'https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg',
+// };
