302df6c5112a18129d5adb646b265760e63e7d2f
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-09-02 07:02:33 +0200

    :sparkles: add error handling

--- part-5/section-34/the-wild-oasis-website/app/_lib/data-service.js
+++ part-5/section-34/the-wild-oasis-website/app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from 'date-fns';
 import { supabase } from './supabase';
+import { notFound } from 'next/navigation';
 
 /////////////
 // GET
@@ -16,6 +17,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
--- part-5/section-34/the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ part-5/section-34/the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -2,18 +2,10 @@ import { getCabin } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
-// // PLACEHOLDER DATA
-// const cabin = {
-//   id: 89,
-//   name: '001',
-//   maxCapacity: 2,
-//   regularPrice: 250,
-//   discount: 0,
-//   description:
-//     'Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.',
-//   image:
-//     'https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg',
-// };
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin - ${name}` };
+}
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
@@ -73,3 +65,16 @@ export default async function Page({ params }) {
     </div>
   );
 }
+
+// // PLACEHOLDER DATA
+// const cabin = {
+//   id: 89,
+//   name: '001',
+//   maxCapacity: 2,
+//   regularPrice: 250,
+//   discount: 0,
+//   description:
+//     'Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.',
+//   image:
+//     'https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg',
+// };
