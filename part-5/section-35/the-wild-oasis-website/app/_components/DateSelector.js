5f3a35ed38ff0d752ff748bdcac9f65c866f5982
Ina F. Pedersen <121164432+InaFPedersen-Work@users.noreply.github.com>
2024-09-03 09:01:03 +0200

    :recycle: refactor code into different components splitting server components from client components

--- part-5/section-35/the-wild-oasis-website/app/_components/DateSelector.js
+++ part-5/section-35/the-wild-oasis-website/app/_components/DateSelector.js
@@ -1,6 +1,8 @@
-import { isWithinInterval } from "date-fns";
-import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
+'use client';
+
+import { isWithinInterval } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -12,7 +14,7 @@ function isAlreadyBooked(range, datesArr) {
   );
 }
 
-function DateSelector() {
+function DateSelector({ settings, cabin, bookedDates }) {
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
@@ -21,8 +23,7 @@ function DateSelector() {
   const range = { from: null, to: null };
 
   // SETTINGS
-  const minBookingLength = 1;
-  const maxBookingLength = 23;
+  const { minBookingLength, maxBookingLength } = settings;
 
   return (
     <div className="flex flex-col justify-between">
@@ -59,7 +60,7 @@ function DateSelector() {
                 <span>&times;</span> <span>{numNights}</span>
               </p>
               <p>
-                <span className="text-lg font-bold uppercase">Total</span>{" "}
+                <span className="text-lg font-bold uppercase">Total</span>{' '}
                 <span className="text-2xl font-semibold">${cabinPrice}</span>
               </p>
             </>
